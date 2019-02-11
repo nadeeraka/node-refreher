0a14ec77e055da63862721fc14a4269e6322519a
nadeeraka <nimanthanadeeraka@outlook.com>
2019-02-11 13:35:28 +0530

    3  commit

--- routes/routes.js
+++ routes/routes.js
@@ -6,7 +6,11 @@ const router = express.Router();
 // });
 
 router.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { title: "Home" });
+});
+
+router.use((req, res) => {
+  res.render("404", { title: "Error" });
 });
 
 module.exports = router;
