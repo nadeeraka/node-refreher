d11340e48ac662c34600d87191ad8377efdea6dd
nadeeraka <nimanthanadeeraka@outlook.com>
2019-02-11 13:19:44 +0530

    second  commit

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const router = require("./routes/routes");
 
 app.use(router);
 
+// set the view engine to ejs
+app.set("view engine", "ejs");
+
 const port = process.env.port || 8080;
 app.listen(port, () => {
   console.log(`App listening on port ${port} !`);
